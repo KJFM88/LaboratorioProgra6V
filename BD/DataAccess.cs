3b394d10b52a2a0fd27313621e1f15f94d6eacf9
KADIR JOCEF FLORES <kfloresm0865@uvirtual.ac.cr>
2022-02-23 17:00:25 -0600

    subir cambios

---
+++ BD/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration configuration;
+
+        public DataAccess(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        public SqlConnection DbConnetion => new SqlConnection(
+            new SqlConnectionStringBuilder(configuration.GetConnectionString("Conn")).ConnectionString 
+            );
+
+        public async Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param= null, int? Timeout=null)
+        {
+            try
+            {
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
